e95ef157cc0999a84b2e03f82a0d29dbe79f222c
loskang96 <gksthf9561@gmail.com>
2024-12-05 14:29:05 +0900

    [zara]

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        './pages/**/*.{js,jsx,ts,tsx}',
+        './components/**/*.{js,jsx,ts,tsx}',
+        './app/**/*.{js,jsx,ts,tsx}', // Next.js에서 사용하는 경우 추가
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'zara-black': '#000000',
+                'zara-gray': '#E5E5E5',
+            },
+            fontFamily: {
+                zara: ['Helvetica', 'Arial', 'sans-serif'],
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
